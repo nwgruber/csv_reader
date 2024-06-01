bd63e76fe9056077fb1a4d30b318eb5723d0cd25
nwgruber <ngruber1998@gmail.com>
2024-06-01 10:39:21 -0700

    added a lil documentation and made var names pythonic

--- csv_reader.py
+++ csv_reader.py
@@ -18,7 +18,7 @@ class MultiPlotFigure(FigureCanvasQTAgg):
         self.axes = fig.add_subplot(111)
         self.axes2 = self.axes.twinx()
         self.axes_refs = [self.axes, self.axes2]
-        super(MultiPlotFigure, self).__init__(fig)
+        super().__init__(fig)
         self.axes.set_xlabel('Time (sec)')
         if bool(title):
             self.axes.set_title(title)
@@ -26,22 +26,22 @@ class MultiPlotFigure(FigureCanvasQTAgg):
         self._plot_refs = [None, None]
         self.colors = ['b', 'r']
 
-    def plot_index(self, xdata, ydata, fig_num, y_text = '', legend_text = ''):
+    def plot_index(self, xdata: pd.Series, ydata: pd.Series, fig_num: int, y_text: str = '', legend_text: str = ''):
         current_ax = self.axes_refs[fig_num]
         if self._plot_refs[fig_num] is None:
-            self._plot_refs[fig_num] = current_ax.plot(xdata, ydata, self.colors[fig_num])[0]
+            self._plot_refs[fig_num] = current_ax.plot(xdata.values, ydata.values, self.colors[fig_num])[0]
         else:
-            self._plot_refs[fig_num].set_ydata(ydata)
+            self._plot_refs[fig_num].set_ydata(ydata.values)
         if bool(y_text):
             current_ax.set_ylabel(y_text)
         if bool(legend_text):
             self._plot_refs[fig_num].set_label(legend_text)
             lines1, labels1 = self.axes.get_legend_handles_labels()
             lines2, labels2 = self.axes2.get_legend_handles_labels()
-            self.axes.legend(lines1 + lines2, labels1 + labels2, loc = 0)
+            self.axes.legend(lines1 + lines2, labels1 + labels2, loc=0)
         self.draw()
 
-    def clear_plot(self, fig_num):
+    def clear_plot(self, fig_num: int):
         if fig_num != -1:
             # Clear one plot
             self.axes_refs[fig_num].cla()
@@ -57,8 +57,8 @@ class MultiPlotFigure(FigureCanvasQTAgg):
         self.draw()
 
 class DoubleLineEdit(QLineEdit):
-    def __init__(self, lower, upper, decimals, parent = None):
-        super(DoubleLineEdit, self).__init__(parent)
+    def __init__(self, lower: float, upper: float, decimals: int, parent=None):
+        super().__init__(parent)
         self.double_validator = QDoubleValidator(lower, upper, decimals)
         self.setValidator(self.double_validator)
 
@@ -81,8 +81,8 @@ class DoubleLineEdit(QLineEdit):
         return float(self.text())
 
 class ErrorMsg(QMessageBox):
-    def __init__(self, msgtext, infotext = '', parent = None):
-        super(ErrorMsg, self).__init__(parent)
+    def __init__(self, msgtext: str, infotext: str = '', parent=None):
+        super().__init__(parent)
         self.setIcon(QMessageBox.Critical)
         self.setWindowTitle('Error')
         self.setText(msgtext)
@@ -92,7 +92,7 @@ class ErrorMsg(QMessageBox):
 
 class PullPlot(QDialog):
     def __init__(self, df: pd.DataFrame, fig_title, parent = None):
-        super(PullPlot, self).__init__(parent)
+        super().__init__(parent)
         self.fig_title = fig_title
         self.setWindowTitle(fig_title)
         self.x_values = df['Time (sec)']
@@ -107,64 +107,64 @@ class PullPlot(QDialog):
             }
         self.col_names = col_names
         self.active_plots = [None, None]
-        self.createMainLayout()
+        self.create_main_layout()
 
-    def createMainLayout(self):
-        mainLayout = QHBoxLayout()
-        leftLayout = QGroupBox('Curves')
-        leftLayout.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
-        leftWrapLayout = QVBoxLayout()
-        leftScrollBoxLayout = QVBoxLayout()
-        leftScrollBox = QScrollArea()
-        scrollContent = QWidget()
+    def create_main_layout(self):
+        main_layout = QHBoxLayout()
+        left_layout = QGroupBox('Curves')
+        left_layout.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
+        left_wrap_layout = QVBoxLayout()
+        left_scrollbox_layout = QVBoxLayout()
+        left_scrollbox = QScrollArea()
+        scroll_content = QWidget()
 
-        self.colCheckBoxes = [QCheckBox(col) for col in self.col_names.keys()]
-        [x.stateChanged.connect(self.curveCheckBoxChanged) for x in self.colCheckBoxes]
-        [leftScrollBoxLayout.addWidget(x) for x in self.colCheckBoxes]
+        self.col_checkboxes = [QCheckBox(col) for col in self.col_names.keys()]
+        [x.stateChanged.connect(self.curveCheckBoxChanged) for x in self.col_checkboxes]
+        [left_scrollbox_layout.addWidget(x) for x in self.col_checkboxes]
 
-        scrollContent.setLayout(leftScrollBoxLayout)
-        leftScrollBox.setWidget(scrollContent)
-        leftWrapLayout.addWidget(leftScrollBox)
+        scroll_content.setLayout(left_scrollbox_layout)
+        left_scrollbox.setWidget(scroll_content)
+        left_wrap_layout.addWidget(left_scrollbox)
 
-        leftLayout.setLayout(leftWrapLayout)
+        left_layout.setLayout(left_wrap_layout)
 
-        rightWidget = QWidget()
-        rightLayout = QVBoxLayout()
-        self.plotWidget = MultiPlotFigure(title = self.fig_title)
-        toolbar = NavigationToolbar(self.plotWidget, self)
+        right_widget = QWidget()
+        right_layout = QVBoxLayout()
+        self.plot_widget = MultiPlotFigure(title = self.fig_title)
+        toolbar = NavigationToolbar(self.plot_widget, self)
         unwanted_btns = ['Subplots']
         for x in toolbar.actions():
             if x.text() in unwanted_btns:
                 toolbar.removeAction(x)
-        rightLayout.addWidget(toolbar)
-        rightLayout.addWidget(self.plotWidget)
-        rightWidget.setLayout(rightLayout)
+        right_layout.addWidget(toolbar)
+        right_layout.addWidget(self.plot_widget)
+        right_widget.setLayout(right_layout)
 
-        mainLayout.addWidget(leftLayout)
-        mainLayout.addWidget(rightWidget)
-        self.setLayout(mainLayout)
+        main_layout.addWidget(left_layout)
+        main_layout.addWidget(right_widget)
+        self.setLayout(main_layout)
 
     def curveCheckBoxChanged(self):
         current_widget = self.sender()
         requested_plot = current_widget.text()
-        widget_states = [x.isChecked() for x in self.colCheckBoxes]
+        widget_states = [x.isChecked() for x in self.col_checkboxes]
         if widget_states.count(True) > 1:
             # Two boxes checked, disable others
-            [x.setDisabled(True) for x in self.colCheckBoxes if not x.isChecked()]
+            [x.setDisabled(True) for x in self.col_checkboxes if not x.isChecked()]
         else:
             # Enable all checkboxes
-            [x.setDisabled(False) for x in self.colCheckBoxes]
+            [x.setDisabled(False) for x in self.col_checkboxes]
         last_plots = self.active_plots
-        requested_plots = [x for x in self.colCheckBoxes if x.isChecked()]
+        requested_plots = [x for x in self.col_checkboxes if x.isChecked()]
         if len(requested_plots) == 0:
             # Removing all plots
-            self.plotWidget.clear_plot(-1)
+            self.plot_widget.clear_plot(-1)
             self.active_plots = [None, None]
         elif len(requested_plots) > len(list(filter(bool, last_plots))):
             # Add an additional plot
             plot_index = last_plots.index(None)
             self.active_plots[plot_index] = requested_plot
-            self.plotWidget.plot_index(self.x_values, self.df[requested_plot], plot_index, y_text = requested_plot, legend_text = self.col_names[requested_plot]['name'])
+            self.plot_widget.plot_index(self.x_values, self.df[requested_plot], plot_index, y_text = requested_plot, legend_text = self.col_names[requested_plot]['name'])
         else:
             # Removing a plot
             plot_index = last_plots.index(requested_plot)
@@ -172,13 +172,13 @@ class PullPlot(QDialog):
             if plot_index == 0:
                 self.active_plots = [self.active_plots[1], None]
             remaining_plot = self.active_plots[0]
-            self.plotWidget.clear_plot(-1)
-            self.plotWidget.plot_index(self.x_values, self.df[remaining_plot], 0, y_text = remaining_plot, legend_text = self.col_names[remaining_plot]['name'])
+            self.plot_widget.clear_plot(-1)
+            self.plot_widget.plot_index(self.x_values, self.df[remaining_plot], 0, y_text = remaining_plot, legend_text = self.col_names[remaining_plot]['name'])
 
 class WidgetGallery(QWidget):
     def __init__(self, parent = None):
-        super(WidgetGallery, self).__init__(parent)
-        self.createMainLayout()
+        super().__init__(parent)
+        self.create_main_layout()
         self.presscount = 0
         self.datalogfile = ''
         self.settings = QSettings('nwgruber', 'Datalog Reader')
@@ -187,57 +187,57 @@ class WidgetGallery(QWidget):
         else:
             self.last_log_dir = ''
 
-    def createMainLayout(self):
-        mainLayout = QVBoxLayout()
-        self.mainWidget = QTabWidget()
+    def create_main_layout(self):
+        main_layout = QVBoxLayout()
+        self.main_widget = QTabWidget()
 
-        self.createStartTab()
-        mainLayout.addWidget(self.mainWidget)
-        self.setLayout(mainLayout)
+        self.create_start_tab()
+        main_layout.addWidget(self.main_widget)
+        self.setLayout(main_layout)
 
-    def createStartTab(self):
-        startTab = QWidget()
-        self.startTabLayout = QVBoxLayout()
+    def create_start_tab(self):
+        start_tab = QWidget()
+        self.start_tab_layout = QVBoxLayout()
         # Start box
-        startBox = QGroupBox('File')
-        startBoxLayout = QVBoxLayout()
-        self.fileLabel = QLabel('No file selected')
-        self.fileBtn = QPushButton('Select a Datalog')
-        self.fileLabel.setBuddy(self.fileBtn)
+        startbox = QGroupBox('File')
+        startbox_layout = QVBoxLayout()
+        self.file_label = QLabel('No file selected')
+        self.file_btn = QPushButton('Select a Datalog')
+        self.file_label.setBuddy(self.file_btn)
 
-        self.fileBtn.clicked.connect(self.fileBtnPressed)
+        self.file_btn.clicked.connect(self.file_btn_pressed)
 
-        startBoxLayout.addWidget(self.fileLabel)
-        startBoxLayout.addWidget(self.fileBtn)
-        startBox.setLayout(startBoxLayout)
+        startbox_layout.addWidget(self.file_label)
+        startbox_layout.addWidget(self.file_btn)
+        startbox.setLayout(startbox_layout)
         # Options box
-        optsBox = QGroupBox('Options')
-        optsBoxLayout = QFormLayout()
+        optsbox = QGroupBox('Options')
+        optsbox_layout = QFormLayout()
 
-        self.throttleInput = DoubleLineEdit(1.0, 100.0, 2)
-        self.throttleInput.setToolTip('Omit data when throttle is below this number')
-        self.throttleInput.setText('50')
+        self.throttle_input = DoubleLineEdit(1.0, 100.0, 2)
+        self.throttle_input.setToolTip('Omit data when throttle is below this number')
+        self.throttle_input.setText('50')
 
-        self.timeFilterInput = DoubleLineEdit(0.001, 10.0, 3)
-        self.timeFilterInput.setToolTip('Omit pulls whose duration is less than this number')
-        self.timeFilterInput.setText('0.5')
+        self.time_filter_input = DoubleLineEdit(0.001, 10.0, 3)
+        self.time_filter_input.setToolTip('Omit pulls whose duration is less than this number')
+        self.time_filter_input.setText('0.5')
 
-        self.startBtn = QPushButton('Start')
-        self.startBtn.clicked.connect(self.startBtnPressed)
+        self.start_btn = QPushButton('Start')
+        self.start_btn.clicked.connect(self.start_btn_pressed)
 
-        self.optsBoxDisabled(True)
-        optsBoxLayout.addRow('Throttle Threshold:', self.throttleInput)
-        optsBoxLayout.addRow('Time Filter:', self.timeFilterInput)
-        optsBoxLayout.addRow(self.startBtn)
-        optsBox.setLayout(optsBoxLayout)
+        self.optsbox_disabled(True)
+        optsbox_layout.addRow('Throttle Threshold:', self.throttle_input)
+        optsbox_layout.addRow('Time Filter:', self.time_filter_input)
+        optsbox_layout.addRow(self.start_btn)
+        optsbox.setLayout(optsbox_layout)
 
-        self.startTabLayout.addWidget(startBox)
-        self.startTabLayout.addWidget(optsBox)
-        startTab.setLayout(self.startTabLayout)
+        self.start_tab_layout.addWidget(startbox)
+        self.start_tab_layout.addWidget(optsbox)
+        start_tab.setLayout(self.start_tab_layout)
 
-        self.mainWidget.addTab(startTab, 'Start')
+        self.main_widget.addTab(start_tab, 'Start')
 
-    def fileBtnPressed(self):
+    def file_btn_pressed(self):
         dialog = QFileDialog()
         dialog.setFileMode(QFileDialog.ExistingFile)
         dialog.setNameFilter('Datalogs (*.csv)')
@@ -246,78 +246,78 @@ class WidgetGallery(QWidget):
 
         if dialog.exec_():
             self.datalogfile = dialog.selectedFiles()[0]
-            self.fileLabel.setText('File: ' + self.datalogfile.rpartition('/')[2])
+            self.file_label.setText('File: ' + self.datalogfile.rpartition('/')[2])
             self.settings.setValue('last_log_dir', self.datalogfile.rpartition('/')[0])
-            self.optsBoxDisabled(False)
+            self.optsbox_disabled(False)
 
-    def startBtnPressed(self):
+    def start_btn_pressed(self):
         [self.datalog, self.ap_info] = read_datalog(self.datalogfile)
-        self.throttleInput.validate_input()
-        self.timeFilterInput.validate_input()
+        self.throttle_input.validate_input()
+        self.time_filter_input.validate_input()
         # Get pulls
-        self.pulls = get_pulls(self.datalog, self.throttleInput.value(), self.timeFilterInput.value())
+        self.pulls = get_pulls(self.datalog, self.throttle_input.value(), self.time_filter_input.value())
         if bool(self.pulls):
             # create second tab
             self.pull_info = get_pull_info(self.pulls)
-            if self.mainWidget.count() == 1:
-                self.createGraphTab()
+            if self.main_widget.count() == 1:
+                self.create_graphtab()
             else:
-                self.updateGraphTab()
-            self.mainWidget.setCurrentIndex(1)
+                self.update_graphtab()
+            self.main_widget.setCurrentIndex(1)
         else:
             msg = ErrorMsg('No pulls found in datalog.', infotext = 'Your throttle threshold is too low or time filter too high.')
             msg.exec_()
 
 
-    def optsBoxDisabled(self, disabled):
-        self.throttleInput.setDisabled(disabled)
-        self.timeFilterInput.setDisabled(disabled)
-        self.startBtn.setDisabled(disabled)
+    def optsbox_disabled(self, disabled):
+        self.throttle_input.setDisabled(disabled)
+        self.time_filter_input.setDisabled(disabled)
+        self.start_btn.setDisabled(disabled)
 
-    def createGraphTab(self):
-        self.graphTab = QWidget()
-        graphTabLayout = QVBoxLayout()
-        graphTabBox = QGroupBox('Create Plot')
-        graphTabBoxLayout = QFormLayout()
+    def create_graphtab(self):
+        self.graphtab = QWidget()
+        graph_tab_layout = QVBoxLayout()
+        graph_tabbox = QGroupBox('Create Plot')
+        graph_tabbox_layout = QFormLayout()
 
-        self.pullPicker = QComboBox()
-        self.pullStartLabel = QLabel()
-        self.pullDurationLabel = QLabel()
-        self.plotBtn = QPushButton('Plot')
-        self.plotBtn.clicked.connect(self.plotBtnPressed)
-        self.updateGraphTab()
-        self.pullPicker.activated.connect(self.pullPickerChanged)
+        self.pull_picker = QComboBox()
+        self.pull_start_label = QLabel()
+        self.pull_duration_label = QLabel()
+        self.plot_btn = QPushButton('Plot')
+        self.plot_btn.clicked.connect(self.plot_btn_pressed)
+        self.update_graphtab()
+        self.pull_picker.activated.connect(self.pull_picker_changed)
 
-        graphTabBoxLayout.addRow('Pull:', self.pullPicker)
-        graphTabBoxLayout.addRow(self.pullStartLabel)
-        graphTabBoxLayout.addRow(self.pullDurationLabel)
-        graphTabBoxLayout.addRow(self.plotBtn)
-        graphTabBox.setLayout(graphTabBoxLayout)
-        graphTabLayout.addWidget(graphTabBox)
-        self.graphTab.setLayout(graphTabLayout)
-        self.mainWidget.addTab(self.graphTab, 'Graph')
+        graph_tabbox_layout.addRow('Pull:', self.pull_picker)
+        graph_tabbox_layout.addRow(self.pull_start_label)
+        graph_tabbox_layout.addRow(self.pull_duration_label)
+        graph_tabbox_layout.addRow(self.plot_btn)
+        graph_tabbox.setLayout(graph_tabbox_layout)
+        graph_tab_layout.addWidget(graph_tabbox)
+        self.graphtab.setLayout(graph_tab_layout)
+        self.main_widget.addTab(self.graphtab, 'Graph')
 
-    def updateGraphTab(self):
+    def update_graphtab(self):
         formspec = '{:<.2f}'
-        pullPickerItems = [str(x) for x in self.pull_info.keys()]
-        self.pullPicker.clear()
-        self.pullPicker.addItems(pullPickerItems)
+        pull_picker_items = [str(x) for x in self.pull_info.keys()]
+        self.pull_picker.clear()
+        self.pull_picker.addItems(pull_picker_items)
         start = formspec.format(self.pull_info[1]['start'])
         duration = formspec.format(self.pull_info[1]['duration'])
-        self.pullStartLabel.setText(f'Start: {start} sec')
-        self.pullDurationLabel.setText(f'Duration: {duration} sec')
+        self.pull_start_label.setText(f'Start: {start} sec')
+        self.pull_duration_label.setText(f'Duration: {duration} sec')
 
-    def pullPickerChanged(self):
+    def pull_picker_changed(self):
         formspec = '{:<.2f}'
-        i = self.pullPicker.currentIndex()
+        i = self.pull_picker.currentIndex()
         selected_pull = self.pull_info[i + 1]
         start = formspec.format(selected_pull['start'])
         duration = formspec.format(selected_pull['duration'])
-        self.pullStartLabel.setText(f'Start: {start} sec')
-        self.pullDurationLabel.setText(f'Duration: {duration} sec')
+        self.pull_start_label.setText(f'Start: {start} sec')
+        self.pull_duration_label.setText(f'Duration: {duration} sec')
 
-    def plotBtnPressed(self):
-        selected_pull = self.pullPicker.currentIndex()
+    def plot_btn_pressed(self):
+        selected_pull = self.pull_picker.currentIndex()
         pull_df = self.pulls[selected_pull]
         fig_title = 'Pull ' + str(selected_pull + 1)
         testplot = PullPlot(pull_df, fig_title)
